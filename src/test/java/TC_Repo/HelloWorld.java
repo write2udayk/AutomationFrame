5de66b2e1654b01b62ed7231a57fc348c408faff
MY PC <MY PC@SNUFFY>
2024-09-14 17:20:40 +0530

    helloWorld class by uday

---
+++ src/test/java/TC_Repo/HelloWorld.java
@@ -0,0 +1,8 @@
+package TC_Repo;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println();
+	}
+
+}
--- src/test/java/TC_Repo/Sample_Prgram_TE_02.java
+++ src/test/java/TC_Repo/Sample_Prgram_TE_02.java
@@ -1,5 +1,7 @@
 package TC_Repo;
 
 public class Sample_Prgram_TE_02 {
-
+	private void syso() {
+		System.out.println("hello");
+	}
 }
